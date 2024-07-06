98d5cb3dd9a81a4b9a7b860fcae6621068215abc
jiwon <semanticist0@gmail.com>
2024-07-06 15:32:13 +0900

    ✅

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,146 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import "./App.css";
+import { createQueryFactory } from "../submodules/simple-querykey-factory/dist";
+
+const baseKey = createQueryFactory("test");
 
 function App() {
-  const [count, setCount] = useState(0)
+  const client = useQueryClient();
+
+  const query1 = useQuery({
+    queryKey: baseKey.all(),
+    queryFn: mockAsync1,
+  });
+
+  const query2 = useQuery({
+    queryKey: baseKey.lists(),
+    queryFn: mockAsync1,
+  });
+
+  const query3 = useQuery({
+    queryKey: baseKey.list("test"),
+    queryFn: mockAsync1,
+  });
+
+  const query4 = useQuery({
+    queryKey: baseKey.details(),
+    queryFn: mockAsync1,
+  });
+
+  const query5 = useQuery({
+    queryKey: baseKey.detail("test"),
+    queryFn: mockAsync1,
+  });
+
+  const query6 = useQuery({
+    queryKey: baseKey.actions(),
+    queryFn: mockAsync1,
+  });
+
+  const query7 = useQuery({
+    queryKey: baseKey.action("test"),
+    queryFn: mockAsync1,
+  });
+
+  const query8 = useQuery({
+    queryKey: baseKey.lists().action("test"),
+    queryFn: mockAsync1,
+  });
+
+  const query9 = useQuery({
+    queryKey: baseKey.lists().detail("test").action("test"),
+    queryFn: mockAsync1,
+  });
+
+  const query91 = useQuery({
+    queryKey: baseKey.list("test").detail("t2e2s2t"),
+    queryFn: mockAsync1,
+  });
+
+  const query10 = useQuery({
+    queryKey: baseKey.lists().action("test").params({
+      test1: "test",
+      test2: "test",
+    }),
+    queryFn: mockAsync1,
+  });
+
+  const query11 = useQuery({
+    queryKey: baseKey.lists().params({
+      test1: "test",
+      test2: "test",
+    }),
+    queryFn: mockAsync1,
+  });
+
+  const query12 = useQuery({
+    queryKey: baseKey.action("test").params(4),
+    queryFn: mockAsync1,
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "start",
+        gap: "10px",
+        height: "100dvh",
+      }}
+    >
+      <button
+        onClick={() => client.invalidateQueries({ queryKey: baseKey.all() })}
+      >
+        모두 초기화
+      </button>
+      <button
+        onClick={() => client.invalidateQueries({ queryKey: baseKey.lists() })}
+      >
+        리스트 초기화
+      </button>
+      <button
+        onClick={() =>
+          client.invalidateQueries({ queryKey: baseKey.list("test") })
+        }
+      >
+        리스트 'test' 초기화
+      </button>
+
+      <button
+        onClick={() =>
+          client.invalidateQueries({ queryKey: baseKey.details() })
+        }
+      >
+        디테일 초기화
+      </button>
+
+      <button
+        onClick={() =>
+          client.invalidateQueries({ queryKey: baseKey.action("test") })
+        }
+      >
+        액션 'test' 초기화
+      </button>
+
+      <button
+        onClick={() =>
+          client.invalidateQueries({ queryKey: baseKey.actions() })
+        }
+      >
+        액션 초기화
+      </button>
+    </div>
+  );
 }
 
-export default App
+const mockAsync1 = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello World");
+    }, 1500);
+  });
+};
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: Infinity,
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-right" />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
